d6a4fc8c583fa6d28f1300868f7f51c82324ce87
bosadnik <bartlomiej@osadnik.com>
2016-10-20 23:12:35 +0000

    another comit

--- app/CommentBox.js
+++ app/CommentBox.js
@@ -1,9 +1,71 @@
 import React from 'react'
+
+
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+let CommentsForm = React.createClass({
+    render: ()=>{
+        return(
+            <div>
+                FORM
+            </div>
+            )
+    }
+});
+
+var Comment = React.createClass({
+    render: function(){
+        return(
+            <div>
+                Author:{this.props.author}, wrote:{this.props.children.toString()}
+            </div>
+        )
+    }
+})
+
+
+let CommentsList = React.createClass({
+    //  getDefaultProps:function(){
+    //     return 
+    //         {data: [
+    //           {id: 1, author: "Pete Hunt", text: "This is one comment"},
+    //           {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+    //         ]}
+                    
+    // },
+    render: function(){
+        var coments = "";
+        try{
+           coments = this.props.data.map((item)=>{
+            return(
+                <Comment author={item.author}>{item.text}</Comment>
+            )
+        })
+            
+        }
+        catch(e){
+            console.log('no data:'+e.message);    
+        }
+        
+        return(
+            <div>
+                {coments}
+            </div>
+            )
+    }
+});
+
 const CommentBox = React.createClass({
-    render: () => {
+   
+    render: function(){
         return (
             <div>
-                hello i'm comment box'a :) from fajla
+                <h1>Comments</h1>
+                <CommentsForm/>
+                <CommentsList data={data}/>
             </div>
         )
     }
@@ -11,4 +73,3 @@ const CommentBox = React.createClass({
 });
 
 export default CommentBox;
-export var pi = 3.14;
