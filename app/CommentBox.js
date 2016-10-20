56f1777e737c05793245132f03c9757d0ea32d2a
bosadnik <bartlomiej@osadnik.com>
2016-10-20 18:55:39 +0000

    working-jspm-react-jsx

---
+++ app/CommentBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const CommentBox = React.createClass({
+    render: () => {
+        return (
+            <div>
+                hello i'm comment box'a :) from fajla
+            </div>
+        )
+    }
+
+});
+
+export default CommentBox;
+export var pi = 3.14;
---
+++ app/index.js
@@ -0,0 +1,11 @@
+console.log('ihaha!');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentBox from  './CommentBox'
+
+
+
+ReactDOM.render(
+    React.createElement(CommentBox),
+    document.getElementById('app')
+);
